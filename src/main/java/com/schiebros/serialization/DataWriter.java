f34a33fa59e0df4623dd0b484f2b9893228a6a69
LakkieGaming <Diego@LAKKIEGAMING>
2017-04-13 17:11:22 -0400

    Created class for writing data

---
+++ src/main/java/com/schiebros/serialization/DataWriter.java
@@ -0,0 +1,35 @@
+package com.schiebros.serialization;
+
+public class DataWriter {
+
+	public static final byte[] getBytes(short value) {
+		return new byte[] { (byte) (value << 8 * 1), (byte) (value) };
+	}
+
+	public static final byte[] getBytes(char value) {
+		return new byte[] { (byte) (value << 8 * 1), (byte) (value) };
+	}
+
+	public static final byte[] getBytes(int value) {
+		return new byte[] { (byte) (value << 8 * 3), (byte) (value << 8 * 2), (byte) (value << 8 * 1), (byte) (value) };
+	}
+
+	public static final byte[] getBytes(long value) {
+		return new byte[] { (byte) (value << 8 * 7), (byte) (value << 8 * 6), (byte) (value << 8 * 5),
+				(byte) (value << 8 * 4), (byte) (value << 8 * 3), (byte) (value << 8 * 2), (byte) (value << 8 * 1),
+				(byte) (value) };
+	}
+	
+	// floating point numbers
+	
+	public static final byte[] getBytes(float value) {
+		int integerValue = Float.floatToIntBits(value);
+		return getBytes(integerValue);
+	}
+	
+	public static final byte[] getBytes(double value) {
+		long longValue = Double.doubleToLongBits(value);
+		return getBytes(longValue);
+	}
+
+}
--- src/test/java/com/schiebros/serialization/test/TestMain.java
+++ src/test/java/com/schiebros/serialization/test/TestMain.java
@@ -1,9 +1,32 @@
 package com.schiebros.serialization.test;
 
+import com.schiebros.serialization.DataWriter;
+
 public class TestMain {
 
-	public static void main(String[] args) {
-		System.out.println("Hello World!");
+	public static void printBytes(byte[] bytes) {
+		for (int i = 0; i < bytes.length; i++) {
+			System.out.format("0x%x ", bytes[i]);
+		}
+		System.out.println();
 	}
-	
+
+	public static void main(String[] args) {
+		System.out.println("Converting 87 to bytes . . .");
+
+		byte[] b0 = DataWriter.getBytes(87);
+		byte[] b1 = DataWriter.getBytes((long) (87));
+		byte[] b2 = DataWriter.getBytes((short) (87));
+
+		System.out.println("Integer");
+		printBytes(b0);
+		
+		System.out.println("Long");
+		printBytes(b1);
+		
+		System.out.println("Short");
+		printBytes(b2);
+
+	}
+
 }
